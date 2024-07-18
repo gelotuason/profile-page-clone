2875fe4088631d62c8b55eb17f6d293bf5b4a379
gelotuason <gelotuason@gmail.com>
2024-07-18 17:23:06 +0800

    changed icon

--- src/app/components/footer/footer-icons.tsx
+++ src/app/components/footer/footer-icons.tsx
@@ -1,16 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faXTwitter, faFacebookMessenger } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF, faXTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import Link from 'next/link';
 
 export default function FooterIcons() {
     const facebook = <FontAwesomeIcon icon={faFacebookF} className="w-4 h-4" />;
     const twitter = <FontAwesomeIcon icon={faXTwitter} className="w-4 h-4" />;
-    const messenger = <FontAwesomeIcon icon={faFacebookMessenger} className='w-4 h-4' />;
+    const whatsapp = <FontAwesomeIcon icon={faWhatsapp} className='w-4 h-4' />;
 
     const links = [
         { href: '/', icon: facebook, key: 'facebook' },
         { href: '/', icon: twitter, key: 'twitter' },
-        { href: '/', icon: messenger, key: 'threads' },
+        { href: '/', icon: whatsapp, key: 'whatsapp' },
     ]
 
     return (
