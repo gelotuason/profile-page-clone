6c6d777c8662050073cd4463a7b8090b99470d3a
gelotuason <gelotuason@gmail.com>
2024-07-18 17:27:56 +0800

    used character entities

--- src/app/components/background/background-desc.tsx
+++ src/app/components/background/background-desc.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundDescription() {
     return (
         <p className="mt-3 tracking-tighter text-gray-500 font-medium">
-            Lorem Ipsum is simply dummy text of the printing and typewriting industry. Lorem Ipsum has been the industry's standard dummy text eversince the 1500s
+            Lorem Ipsum is simply dummy text of the printing and typewriting industry. Lorem Ipsum has been the industry&apos;s standard dummy text eversince the 1500s
         </p>
     )
 }
